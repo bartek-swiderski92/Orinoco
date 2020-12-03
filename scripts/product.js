c4151ab4296fc5f624e957fa276bccc98d490b4e
Phos23 <phos23@gmail.com>
2020-12-03 13:21:45 +0000

    remove forEach from product.js

--- scripts/product.js
+++ scripts/product.js
@@ -2,14 +2,12 @@ const id = document.URL.split("=")[1];
 const itemDisplay = document.getElementById('item-display');
 let basketContent;
 
-function displayItem(items) {
-    itemDisplay.innerHTML = 'Loading...'
-
-    items.forEach(item => {
-        if (id === item._id) {
-            let dollars = priceToDollars(item.price)
-
-            itemDisplay.innerHTML = `<div class="row my-5">
+function displayItem(item) {
+    itemDisplay.innerHTML = 'Loading...';
+    if (id === item._id) {
+        let dollars = priceToDollars(item.price);
+        //generates html
+        itemDisplay.innerHTML = `<div class="row my-5">
                 <div class="col-4 my-5 pb-5">
                     <img src="${item.imageUrl}" alt="${item.name}" class="card-img-top my-4 px-5">
                 </div>
@@ -25,44 +23,49 @@ function displayItem(items) {
                     <button id="add-to-cart" class="btn btn-primary">Add to Cart</button>
 
                 </div>`;
-            // Generates lenses options
-            const lensesOptions = document.querySelector('#lenses');
-            item.lenses.forEach(lens => {
-                const optionElement = document.createElement('option');
-                optionElement.value = lens;
-                optionElement.innerHTML = lens;
-                lensesOptions.appendChild(optionElement);
-            });
-            // Adds listener to Add to Cart Button
-            let addToCartBtn = document.querySelector('#add-to-cart');
+        // Generates lenses options
+        const lensesOptions = document.querySelector('#lenses');
+        item.lenses.forEach(lens => {
+            const optionElement = document.createElement('option');
+            optionElement.value = lens;
+            optionElement.innerHTML = lens;
+            lensesOptions.appendChild(optionElement);
+        });
+        // Adds listener to Add to Cart Button
+        let addToCartBtn = document.querySelector('#add-to-cart');
 
-            function addToCart() {
-                if (!localStorage.getItem('basket')) {
-                    basketContent = [];
-                } else {
-                    basketContent = JSON.parse(localStorage.getItem('basket'))
-                }
-                basketContent.push(item._id);
-                localStorage.setItem('basket', JSON.stringify(basketContent));
+        function addToCart() {
+            if (!localStorage.getItem('basket')) {
+                basketContent = [];
+            } else {
+                basketContent = JSON.parse(localStorage.getItem('basket'))
+            }
+            basketContent.push(item._id);
+            localStorage.setItem('basket', JSON.stringify(basketContent));
 
-                window.location.href = `/cart.html`;
-            };
-            addToCartBtn.addEventListener('click', addToCart);
-        }
-    });
+            window.location.href = `/cart.html`;
+        };
+        addToCartBtn.addEventListener('click', addToCart);
+    } else {
+        unableToLoad();
+    }
+}
+
+function unableToLoad() {
+    itemDisplay.innerHTML = `<i class="fas fa-times-circle pr-3" style="font-size:25px"></i>  Unable to load Content! Please try again later or contact website administrator.`;
+    itemDisplay.classList.add('bg-danger',
+        'text-light', 'text-center', 'mt-5', 'p-3')
 }
 
 async function fetchAndDisplayItem(query) {
     itemDisplay.innerHTML = loadingAnimation;
-
-    await getContent(query).then(items => {
-        displayItem(items);
+    console.log(query);
+    await getContent(query).then(item => {
+        displayItem(item);
     }).catch((error) => {
         console.log(error);
-        itemDisplay.innerHTML = `<i class="fas fa-times-circle pr-3" style="font-size:25px"></i>  Unable to load Content! Please try again later or contact website administrator.`;
-        itemDisplay.classList.add('bg-danger',
-            'text-light', 'text-center', 'mt-5', 'p-3')
+        unableToLoad();
     })
 }
 
-fetchAndDisplayItem(camerasApi);
+fetchAndDisplayItem(camerasApi + id);
